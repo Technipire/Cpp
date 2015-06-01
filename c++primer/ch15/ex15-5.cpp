519bb37bebb772454947d0c44e6446addcf0e63f
Technipire <andyxian510@gmail.com>
2015-06-01 13:45:23 -0400

    ex15-5 for c++ primer added

---
+++ c++primer/ch15/ex15-5.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+class Base {
+    public:
+        void pub_mem();
+    protected:
+        int prot_mem;
+    private:
+        char priv_mem;
+};
+
+struct Pub_Derv : public Base {
+    int f() {return prot_mem;}
+    // char g() {return priv_mem;} //illegal
+    void memfcn (Base &b) { b = *this; }
+};
+
+struct Priv_Derv : private Base {
+    int f1() const { return prot_mem; }
+    void memfcn (Base &b) { b = *this; }
+};
+
+struct Prot_Derv : protected Base {
+    int f1() const { return prot_mem;}
+    void memfcn (Base &b) { b = *this; }
+}; 
+
+struct Derived_from_Public : public Pub_Derv {
+    int use_base() { return prot_mem; }
+    void memfcn (Base &b) { b = *this; }
+};
+
+struct Derived_from_Protected : public Prot_Derv {
+    int use_base() { return prot_mem; };
+    void memfcn (Base &b) { b = *this; }
+};
+
+struct Derived_from_Private : public Priv_Derv {
+    // int use_base() { return prot_mem; } // illegal;
+    char use_base() {return priv_mem;}
+    void memfcn (Base &b) { b = *this; }
+};
+
+int main() {
+    Pub_Derv d1;
+    Priv_Derv d2;
+    Prot_Derv d3;
+    Derived_from_Public dd1;
+    Derived_from_Private dd2;
+    Derived_from_Protected dd3;
+    Base *p = &d1;
+    //p = &d2; // illegal
+    //p = &d3; //illegal
+    p = &dd1;
+    //p = &dd2;
+    //p = &dd3;
+
+
+    return 0;
+
+}
